4f6a82b565b5e8d91cb3bad18bff6cacc36b31fe
M Refat <info.carobara@gmail.com>
2026-04-26 13:21:45 +0200

    fix chromium + pdf

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["@sparticuz/chromium"],
+  serverExternalPackages: [
+    "@sparticuz/chromium",
+    "puppeteer-core",
+  ],
+
+  outputFileTracingIncludes: {
+    "/api/submit-request": [
+      "./node_modules/@sparticuz/chromium/bin/**/*",
+    ],
+    "/api/**/*": [
+      "./node_modules/@sparticuz/chromium/bin/**/*",
+    ],
+  },
 };
 
 export default nextConfig;
